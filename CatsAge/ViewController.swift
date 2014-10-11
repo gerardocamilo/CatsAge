3994634a8b427f690999477e2fc9b2e63755ef14
Gerardo Camilo <gerardocamilo@MacBook-Pro-de-Gerardo.local>
2014-10-11 13:40:46 -0400

    Completed the cat age calculator.

--- CatsAge/ViewController.swift
+++ CatsAge/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var txtHumanAge: UITextField!
+    
+    @IBOutlet weak var lblCatAgeResult: UILabel!
+    
+    @IBAction func calculate(sender: AnyObject) {
+        
+        let tempHumanAge = txtHumanAge.text
+        var result = 0;
+        
+        if  tempHumanAge != nil || !tempHumanAge.isEmpty {
+            
+            let humanAge = txtHumanAge.text.toInt()
+            
+            if humanAge != nil {
+                result = humanAge! * 7
+            }
+        }
+
+        lblCatAgeResult.text = String(result)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
